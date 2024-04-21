fd162ecb320ad08387a8553b6cd891f447fb2a83
Ernesto Canquiz <fdsoil123@gmail.com>
2024-04-21 12:38:04 -0400

    fix: Cross-Origin Request Blocked

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
 // https://www.youtube.com/watch?v=JdrvETQCAGw&list=PLr3d3QYzkw2xabQRUpcZ_IBk9W50M9pe-&index=59&t=462s
 // https://www.youtube.com/watch?v=QiO0uUwOiBg&list=PLr3d3QYzkw2xabQRUpcZ_IBk9W50M9pe-&index=62&t=290s
 require __DIR__ . '/vendor/autoload.php';
-header('Content-type: application/json');
+header("Content-type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
 
 $router = new App\Router();
 $router->get('/test', function () { echo 'Test function'; } );
